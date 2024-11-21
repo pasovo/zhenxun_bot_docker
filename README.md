88602a0bd81621f7bd81f45de32bc71b82755f7d
Starone <415187499@qq.com>
2024-11-21 23:17:11 +0800

    更新readme

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# zhenxun_bot_docker
+# zhenxun_bot_docker 
+来源https://github.com/HibiKier/zhenxun_bot
+
+感谢大佬写出如此可爱的小真寻
+
+代码几乎全部来自于chatgpt
+
+使用此代码默认你会使用docker，不会你问我我也不会（逃
+
+运行逻辑为每6小时检查主仓库Releases更新，所以应该大概也许不会出什么问题（出问题当我没说
+
+### 已知bug：
+
+webui几乎不能使用
+
+~~重启命令有问题~~ 已经修复，现在重启应该不会出问题
+
+其他待测试，有bug提issue，有时间会修的
+
+## 使用方式
+因为我只用docker compose，所以这里也只提供docker compose~
+ ```json
+version: '3.8'
+
+services:
+  zhenxun-docker:
+    image: registry.cn-hangzhou.aliyuncs.com/starfishes/zhenxun-docker
+    ports:
+      - "8080:8080"
+    container_name: zhenxun-docker
+    network_mode: bridge
+    volumes:
+      - ./.env.dev:/app/zhenxun/.env.dev
+      - ./data:/app/zhenxun/data
+      - ./resources:/app/zhenxun/resources 
+      - ./log:/app/zhenxun/log
+      - ./zhenxun/plugins:/app/zhenxun/zhenxun/plugins （插件目录，非必须
+    restart: no
+```
--- restart.sh
+++ restart.sh
@@ -1,29 +1,36 @@
 #!/bin/bash
 
-# 检查是否有占用端口 8989 的进程
-pid=$(netstat -tunlp 2>/dev/null | grep 8989 | awk '{print $7}' | cut -d'/' -f1)
+# 加载 .env.dev 文件中的 PORT 变量
+if [ -f .env.dev ]; then
+    export $(grep -oP '^PORT=\K.+' .env.dev)  # 提取并导出 PORT 变量
+else
+    echo ".env.dev 文件不存在。"
+    exit 1
+fi
+
+# 检查是否占用端口，使用从 .env.dev 加载的 PORT 值
+pid=$(netstat -tunlp 2>/dev/null | grep $PORT | awk '{print $7}' | cut -d'/' -f1)
+
 if [[ -n "$pid" ]]; then
-    echo "正在杀死占用端口 8989 的进程 $pid"
+    echo "正在杀死占用端口 $PORT 的进程 $pid"
     kill -9 $pid
     echo "等待进程 $pid 退出..."
     wait $pid 2>/dev/null || sleep 2  # 确保进程结束或等待一段时间
 else
-    echo "没有进程占用端口 8989"
+    echo "没有进程占用端口 $PORT，好耶"
 fi
 
 # 再次检查端口是否被释放
-if netstat -tunlp 2>/dev/null | grep -q 8989; then
-    echo "端口 8989 仍被占用，无法启动 bot.py"
+if netstat -tunlp 2>/dev/null | grep -q $PORT; then
+    echo "端口 $PORT 仍被占用，重启镜像吧~"
     exit 1
 fi
 
 # 检测是否在虚拟环境中
 if [[ -n "$VIRTUAL_ENV" ]]; then
-    echo "检测到当前在虚拟环境中，直接运行 python3 bot.py..."
-    python3 bot.py 2>&1 | tee bot_error.log
+    python3 bot.py
 else
-    echo "未检测到虚拟环境，使用 poetry 运行 bot.py..."
-    poetry run python3 bot.py 2>&1 | tee bot_error.log
+    poetry run python3 bot.py
 fi
 
 echo "done"
